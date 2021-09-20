0d08765bb7a0c3ef055d3fa08ce9cb448fe2ce22
olegfomenko2002 <oleg.fomenko2002@gmail.com>
2021-09-20 09:22:28 +0300

    refactoring - faster implementing with option take()

--- src/main.rs
+++ src/main.rs
@@ -1,135 +1,137 @@
-use std::cmp::max;
 use std::io::Read;
 
-enum Treap where {
-    Node(i32, i32, usize, Box<Treap>, Box<Treap>),
-    Nil,
+struct Node {
+    x: i32,
+    y: i32,
+    size: usize,
+    left: Option<Box<Node>>,
+    right: Option<Box<Node>>,
 }
 
-impl Treap {
-    pub fn new(x: i32, y: i32, left: Box<Treap>, right: Box<Treap>) -> Treap {
-        let mut t = Treap::Node(x, y, 1, left, right);
-        t.update_size();
-        t
-    }
+pub type Treap = Option<Box<Node>>;
 
-    fn size(&self) -> usize {
-        return match self {
-            Treap::Nil => 0,
-            Treap::Node(_, _, sz, ..) => *sz
-        };
-    }
+pub fn merge(t1: &mut Treap, t2: Treap) {
+    match (t1.take(), t2) {
+        (Some(mut tl), Some(mut tr)) => {
+            if tl.y > tr.y {
+                merge(&mut tl.right, Some(tr));
+                tl.update();
+                *t1 = Some(tl);
+            } else {
+                let mut new_tree = Some(tl);
+                merge(&mut new_tree, tr.left.take());
+                tr.left = new_tree;
+                tr.update();
+                *t1 = Some(tr);
+            }
+        }
 
-    fn update_size(&mut self) -> usize {
-        return match self {
-            Treap::Nil => {
-                0
+        (t, None) | (None, t) => {
+            *t1 = t;
+        }
+    }
+}
+
+pub fn split(t: &mut Treap, k: i32) -> Treap {
+    return match t.take() {
+        Some(mut node) => {
+            let ret;
+
+            if node.x <= k {
+                ret = split(&mut node.right, k);
+                node.update();
+                *t = Some(node);
+            } else {
+                let res = split(&mut node.left, k);
+                *t = node.left.take();
+                node.left = res;
+                node.update();
+                ret = Some(node);
             }
 
-            Treap::Node(_, _, size, left, right) => {
-                *size = 1 + left.size() + right.size();
-                *size
+            ret
+        }
+
+        None => None
+    };
+}
+
+fn push(t: &mut Treap, x: i32, y: i32) {
+    let node = Some(Box::new(
+        Node {
+            x,
+            y,
+            size: 1,
+            left: None,
+            right: None,
+        }
+    ));
+
+    let right = split(t, x);
+    merge(t, node);
+    merge(t, right);
+}
+
+fn get(t: &Treap, k: usize) -> i32 {
+    return match t.as_ref() {
+        None => 0,
+        Some(node) => {
+            let mut sz: usize = 1;
+            if let Some(t) = node.right.as_ref() {
+                sz += t.size;
             }
-        };
-    }
 
-    fn split(self, k: i32) -> (Self, Self) {
-        use Treap::*;
-
-        return match self {
-            Nil => (Nil, Nil),
-            Node(x, y, _, left, right) => {
-                if x <= k {
-                    let (l, r) = right.split(k);
-                    (Treap::new(x, y, left, Box::new(l)), r)
-                } else {
-                    let (l, r) = left.split(k);
-                    (l, Treap::new(x, y, Box::new(r), right))
-                }
+            if sz < k {
+                return get(&node.left, k - sz);
             }
-        };
-    }
 
-    fn merge(t1: Self, t2: Self) -> Self {
-        use Treap::*;
-
-        return match (t1, t2) {
-            (Nil, Nil) => Nil,
-            (t, Nil) => t,
-            (Nil, t) => t,
-            (Node(lx, ly, _, ll, lr), Node(rx, ry, _, rl, rr)) => {
-                if ly > ry {
-                    let t2 = Treap::new(rx, ry, rl, rr);
-                    Treap::new(lx, ly, ll, Box::new(Treap::merge(*lr, t2)))
-                } else {
-                    let t1 = Treap::new(lx, ly, ll, lr);
-                    Treap::new(rx, ry, Box::new(Treap::merge(t1, *rl)), rr)
-                }
+            if sz == k {
+                return node.x;
             }
-        };
-    }
 
-    fn push(self, x: i32, y: i32) -> Self {
-        use Treap::*;
+            return get(&node.right, k);
+        }
+    };
+}
 
-        let node = Treap::new(x, y, Box::new(Treap::Nil), Box::new(Treap::Nil));
-        return match self {
-            Nil => node,
+fn del(t: &mut Treap, k: i32) {
+    match t.take() {
+        None => {}
+        Some(mut node) => {
+            if node.x == k {
+                merge(&mut node.left, node.right);
+                *t = node.left;
+                return;
+            }
+
+            if node.x < k {
+                del(&mut node.right, k);
+            } else {
+                del(&mut node.left, k);
+            }
+
+            node.update();
+            *t = Some(node)
+        }
+    };
+}
+
+
+impl Node {
+    fn update(&mut self) {
+        match (self.left.as_ref(), self.right.as_ref()) {
+            (Some(tl), Some(tr)) => {
+                self.size = 1 + tl.size + tr.size;
+            }
+
+            (Some(t), None) | (None, Some(t)) => {
+                self.size = 1 + t.size;
+            }
+
             _ => {
-                let (l, r) = self.split(x);
-                let left = Treap::merge(l, node);
-                Treap::merge(left, r)
+                self.size = 1;
             }
-        };
-    }
-
-    fn get(&self, k: usize) -> i32 {
-        use Treap::*;
-
-        return match self {
-            Nil => 0,
-            Node(x, _, _, l, r) => {
-                if r.size() + 1 < k {
-                    return l.get(k - r.size() - 1);
-                }
-
-                if r.size() + 1 == k {
-                    return *x;
-                }
-
-                return r.get(k);
-            }
-        };
-    }
-
-    fn del(self, k: i32) -> Self {
-        use Treap::*;
-
-        return match self {
-            Nil => Nil,
-            Node(x, y, sz, l, r) => {
-                if x == k {
-                    return Treap::merge(*l, *r);
-                }
-
-                if x < k {
-                    return Treap::new(x, y, l, Box::new(r.del(k)));
-                }
-
-                return Treap::new(x, y, Box::new(l.del(k)), r);
-            }
-        };
-    }
-
-    fn deep(&self) -> i32 {
-        use Treap::*;
-
-        return match self {
-            Nil => 0,
-            Node(.., l, r) => {
-                max(l.deep(), r.deep()) + 1
-            }
-        };
+        }
     }
 }
 
@@ -153,7 +155,7 @@ fn get_word() -> String {
     }
 }
 
-fn get() -> i32 { get_word().parse().ok().unwrap() }
+fn read() -> i32 { get_word().parse().ok().unwrap() }
 
 const A: i64 = 1366;
 const C: i64 = 150889;
@@ -180,16 +182,17 @@ fn main() {
     let mut random = Random::new();
     random.next();
 
-    let mut treap = Treap::Nil;
-    let mut n: i32 = get();
+    let mut treap = None;
+    let mut n: i32 = read();
+
     for i in 0..n {
-        let op: i32 = get();
-        let val: i32 = get();
+        let op: i32 = read();
+        let val: i32 = read();
 
         match op {
-            1 => treap = treap.push(val, random.next()),
-            0 => println!("{}", treap.get(val as usize)),
-            _ => treap = treap.del(val),
+            1 => push(&mut treap, val, random.next()),
+            0 => println!("{}", get(&treap, val as usize)),
+            _ => del(&mut treap, val),
         }
     }
 }
